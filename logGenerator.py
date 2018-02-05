2c1fa1b51221a5becaa7e717504fa24fc62bcfa6
Jonathan Cooper <jonatcooper@gmail.com>
2018-02-05 16:38:16 -0600

    Initial commit of project code.

---
+++ logGenerator.py
@@ -0,0 +1,30 @@
+#
+
+import psycopg2
+
+# set up the strings for the views and queries that will collect the data for all the questions.
+# Strings to hold the views that are created for each question.
+view_q1 = """
+"""
+view_q2 = """
+"""
+view_q3 = """
+"""
+
+# Strings to hold the sql queries to extract the information for each question.
+query_q1 = """
+"""
+query_q2 = """
+"""
+query_q3 = """
+"""
+
+# Strings to hold the returned data from each query.
+output_q1 = """
+"""
+output_q2 = """
+"""
+output_q3 = """
+"""
+
+# 
